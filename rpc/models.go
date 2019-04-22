4e27e9aed58a6cb9eb5a3743cb17c9c7715ad8a5
Company <yiuked@vip.qq.com>
2019-04-22 17:48:36 +0800

    add sms

--- rpc/models.go
+++ rpc/models.go
@@ -1,10 +1,13 @@
 package main
 
+import "github.com/jinzhu/gorm"
+
+// 考题类型
 type Type struct {
 	Title string
 	ExamineId int
 }
-
+// 考题
 type Question struct {
 	QuestionId int
 	ExamineId int
@@ -17,12 +20,32 @@ type Question struct {
 	ItemD string
 }
 
+// 用户
+type User struct {
+	gorm.Model
+	UserId int
+	NickName string
+	Phone string
+	Password string
+	Identity string
+}
+
+// 用户
+type Sms struct {
+	gorm.Model
+	Phone string
+	Status bool
+	Msg string
+}
+
+// 返回信息格式
 type Response struct {
 	RespCode int
 	RespDesc string
 	RespData interface{}
 }
 
+// 还回数组格式
 type Item struct {
 	Limit int
 	Offset int
