3ab392b413ae8566a0b598e30d136e6569f91a67
intelPC <Administrator@DEEP-1804282302.(none)>
2019-06-16 22:12:50 +0800

    test api

--- rpc/types.go
+++ rpc/types.go
@@ -3,15 +3,22 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
 
 func GetTypes(c *gin.Context) {
 	db := Conn()
 	defer db.Close()
 
+	limit, _ := strconv.Atoi(c.DefaultQuery("limit", "20"))
+	offset, _ := strconv.Atoi(c.DefaultQuery("offset", "0"))
+
 	// 读取
 	var types []Type
+	var Total int
 	db.Find(&types)
+
+	db.Model(&Question{}).Count(&Total).Limit(limit).Offset(offset).Find(&types)
 
 	c.JSON(http.StatusOK, Response{RespCode: RespStatusOK, RespDesc: "Success", RespData: types})
 }
@@ -19,6 +26,8 @@ func GetTypes(c *gin.Context) {
 func CreateType(c *gin.Context) {
 	db := Conn()
 	defer db.Close()
+
+
 
 	// Migrate the schema
 	db.AutoMigrate(&Type{})
